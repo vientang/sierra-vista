ff84fc598799d9753f72b7958dd5d66da2f19585
vien.tang <vien.tang@veeva.com>
2018-12-24 12:31:18 -0800

    Initialize a mutation for creating a user and query for listing all users

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+    createUser(parent, args, context, info) {
+        global.users = global.users || [];
+        const newUser = { first: args.first };
+        global.users.push(newUser);
+        console.log('args', newUser);
+        return newUser;
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const Query = {
+    user(parent, args, context, info) {
+        global.users = global.users || [];
+        // this is where the database calls go
+        // regardless of what database is used
+        return global.users;
+    }
+};
 
 module.exports = Query;
