99f3fde5ee40c7c6c36ee9f7c094be59c98f9ff9
vien.tang <vien.tang@veeva.com>
2018-12-27 17:18:07 -0800

    Add new favicon

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ba45c21e0ffd7488bc8f";
+/******/ 	var hotCurrentHash = "ff2df8ae9f3af2829a94";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
