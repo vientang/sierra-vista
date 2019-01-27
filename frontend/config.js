d50ebf5dc79d47ca34eb1e71c82dbf267df69665
vien.tang <vien.tang@veeva.com>
2019-01-26 18:02:25 -0800

    Add back config for Now deploy

--- frontend/config.js
+++ frontend/config.js
@@ -2,9 +2,3 @@
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint = 'https://sierra-vista-yoga-prod.herokuapp.com/';
 export const perPage = 4;
-
-export const emailJsConfig = {
-    EMAILJS_USERID: "user_5F0gIYOfpVKdI0kCjQoRq",
-    EMAILJS_TEMPLATEID: "rental_gear",
-    EMAILJS_RECEIVER: "vientang@gmail.com",
-}
