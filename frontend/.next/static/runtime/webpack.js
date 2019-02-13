f1b577829c16c413e8cfded4b0db48920b1eda46
vien.tang <vien.tang@veeva.com>
2019-02-12 21:28:13 -0800

    Use a better favicon

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a92d7d553d27624b38e1";
+/******/ 	var hotCurrentHash = "97888265b627af609228";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 /**
  * Add meta tags to site. Next.js handles the side effects.
  */
 const Meta = () => {
-    return (
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
-            <link rel="shortcut icon" href="/static/favicon.ico" />
-            <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-            <title>Sierra Vista</title>
-        </Head>
-    );
-}
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Sierra Vista</title>
+    </Head>
+  );
+};
 
 export default Meta;
